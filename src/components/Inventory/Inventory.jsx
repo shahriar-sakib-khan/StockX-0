a3cfc05948adbd7ccbd91249d97098684ec5522c
Ebtehaj Ahmed <ahmedratul216@gmail.com>
2025-03-13 22:47:34 +0600

    Fix the grid issue in inventory page

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -17,102 +17,95 @@ function Inventory() {
   }
 
   return (
-<div className={styles.inventoryContainer}>
-
-  <nav className={styles.secondaryNavbar}>
-    <ul className={styles.secondaryNavList}>
-      <li className={styles.secondaryNavItems}><a href="#cylinders" className={styles.secondaryNavLink}>Cylinders</a></li>
-      <li className={styles.secondaryNavItems}><a href="#regulators" className={styles.secondaryNavLink}>Regulators</a></li>
-      <li className={styles.secondaryNavItems}><a href="#stoves" className={styles.secondaryNavLink}>Stoves</a></li>
-    </ul>
-    <ul className={styles.secondaryNavList}>
-      <li className={styles.secondaryNavItems}><a href="/empty-cylinders" className={styles.secondaryNavLink}>Empty Cylinders</a></li>
-    </ul>
-  </nav>
-
-  {selectedBrands.length > 0 ? (
-    <div className={styles.sectionHeader}>
-      <h2 id="cylinders">Cylinders</h2>
-      <div className={styles.grid}>
-        {allBrands
-          .filter(brand => selectedBrands.includes(brand.id))
-          .map(brand => (
-            <div key={brand.id} className={styles.brandContainer}>
-              {brand.cylinders.map((cylinder, index) => (
-                <Card
-                  key={`${brand.id}-${cylinder.type}`}
-                  id={brand.id}
-                  name={brand.name}
-                  type={cylinder.type}
-                  picture={cylinder.image}
-                  price={prices[brand.id]?.[cylinder.type] ?? brand.price}
-                  stock={stockCount[brand.id]?.[cylinder.type] ?? brand.stock}
-                  updateStock={updateStock}
-                  />
-                ))}
-            </div>
-          ))}
-      </div>
+  <div className={styles.wrapper}>
+    <div className={styles.secondaryNavbar}>
+      <ul className={styles.secondaryNavList}>
+        <li className={styles.secondaryNavItems}><a href="#cylinders" className={styles.secondaryNavLink}>Cylinders</a></li>
+        <li className={styles.secondaryNavItems}><a href="#regulators" className={styles.secondaryNavLink}>Regulators</a></li>
+        <li className={styles.secondaryNavItems}><a href="#stoves" className={styles.secondaryNavLink}>Stoves</a></li>
+      </ul>
+      <ul className={styles.secondaryNavList}>
+        <li className={styles.secondaryNavItems}><a href="/empty-cylinders" className={styles.secondaryNavLink}>Empty Cylinders</a></li>
+      </ul>
     </div>
-  ) : (
-    <p>No brands selected</p>
-  )}
-  {selectedBrands.length > 0 ? (
-    <div className={styles.sectionHeader}>
-      <h2 id="regulators">Regulators</h2>
-      <div className={styles.grid}>
-        {allBrands
-          .filter(brand => selectedBrands.includes(brand.id))
-          .map(brand => (
-            <div key={brand.id} className={styles.brandContainer}>
-              {brand.cylinders.map((cylinder, index) => (
-                <Card
-                key={`${brand.id}-${cylinder.type}`}
-                id={brand.id}
-                name={brand.name}
-                type={cylinder.type}
-                picture={cylinder.image}
-                price={prices[brand.id]?.[cylinder.type] ?? brand.price}
-                stock={stockCount[brand.id]?.[cylinder.type] ?? brand.stock}
-                updateStock={updateStock}
-                />
-              ))}
-            </div>
-          ))}
-      </div>
-    </div>
-  ) : (
-    <p>No brands selected</p>
-  )}
+
     {selectedBrands.length > 0 ? (
-    <div className={styles.sectionHeader}>
-      <h2 id="stoves">Stoves</h2>
-      <div className={styles.grid}>
-        {allBrands
-          .filter(brand => selectedBrands.includes(brand.id))
-          .map(brand => (
-            <div key={brand.id} className={styles.brandContainer}>
-              {brand.cylinders.map((cylinder, index) => (
-                <Card
-                key={`${brand.id}-${cylinder.type}`}
-                id={brand.id}
-                name={brand.name}
-                type={cylinder.type}
-                picture={cylinder.image}
-                price={prices[brand.id]?.[cylinder.type] ?? brand.price}
-                stock={stockCount[brand.id]?.[cylinder.type] ?? brand.stock}
-                updateStock={updateStock}
-                />
-              ))}
-            </div>
-          ))}
+      <div className={styles.sectionHeader}>
+        <h1 id="cylinders">Cylinders</h1>
+        <div className={styles.grid}>
+          {allBrands
+            .filter(brand => selectedBrands.includes(brand.id))
+            .map(brand => (
+                brand.cylinders.map((cylinder) => (
+                  <Card
+                    key={`${brand.id}-${cylinder.type}`}
+                    id={brand.id}
+                    name={brand.name}
+                    type={cylinder.type}
+                    picture={cylinder.image}
+                    price={prices[brand.id]?.[cylinder.type] ?? brand.price}
+                    stock={stockCount[brand.id]?.[cylinder.type] ?? brand.stock}
+                    updateStock={updateStock}
+                  />
+                ))
+            ))}
+        </div>
       </div>
-    </div>
-  ) : (
-    <p>No brands selected</p>
-  )}
-  <a href="#" className={styles.backToTopBtn}>⬆</a>
-</div>
+    ) : (
+      <p className={styles.noBrands}>No brands selected</p>
+    )}
+    {selectedBrands.length > 0 ? (
+      <div className={styles.sectionHeader}>
+        <h1 id="regulators">Regulators</h1>
+        <div className={styles.grid}>
+        {allBrands
+            .filter(brand => selectedBrands.includes(brand.id))
+            .map(brand => (
+                brand.cylinders.map((cylinder) => (
+                  <Card
+                    key={`${brand.id}-${cylinder.type}`}
+                    id={brand.id}
+                    name={brand.name}
+                    type={cylinder.type}
+                    picture={cylinder.image}
+                    price={prices[brand.id]?.[cylinder.type] ?? brand.price}
+                    stock={stockCount[brand.id]?.[cylinder.type] ?? brand.stock}
+                    updateStock={updateStock}
+                  />
+                ))
+            ))}
+        </div>
+      </div>
+    ) : (
+      <p className={styles.noBrands}>No brands selected</p>
+    )}
+      {selectedBrands.length > 0 ? (
+      <div className={styles.sectionHeader}>
+        <h1 id="stoves">Stoves</h1>
+        <div className={styles.grid}>
+        {allBrands
+            .filter(brand => selectedBrands.includes(brand.id))
+            .map(brand => (
+                brand.cylinders.map((cylinder) => (
+                  <Card
+                    key={`${brand.id}-${cylinder.type}`}
+                    id={brand.id}
+                    name={brand.name}
+                    type={cylinder.type}
+                    picture={cylinder.image}
+                    price={prices[brand.id]?.[cylinder.type] ?? brand.price}
+                    stock={stockCount[brand.id]?.[cylinder.type] ?? brand.stock}
+                    updateStock={updateStock}
+                  />
+                ))
+            ))}
+        </div>
+      </div>
+    ) : (
+      <p className={styles.noBrands}>No brands selected</p>
+    )}
+    <a href="#" className={styles.backToTopBtn}>⬆</a>
+  </div>
   )
 }
 
--- src/components/Selection/Selection.jsx
+++ src/components/Selection/Selection.jsx
@@ -32,43 +32,41 @@ function Selection() {
   const isSubmitDisabled = !(selectedBrands.length > 0);
 
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.titleSection}>
-          <div className={styles.titleSubmit}>
-            <h2 className={styles.title}>Selection Page</h2>
-            <Button
-              className={styles.submitBtn}
-              onClick={() => handleSubmit(isSubmitDisabled)}
-              disabled={isSubmitDisabled}
-              data-tool-tip={"Select a brand to proceed"}
-            >
-              Submit
-            </Button>
-          </div>
-          <div className={styles.selectCount}>
-            <Button
-              onClick={toggleSelect}
-            >
-              {selectedBrands.length === allBrands.length ? 'Deselect All' : 'Select All'}
-            </Button>
-            <span className={styles.counter}>Selected: {selectedBrands.length} / {allBrands.length}</span>
-          </div>
+    <div className={styles.wrapper}>
+      <div className={styles.titleSection}>
+        <div className={styles.titleSubmit}>
+          <h2 className={styles.title}>Selection Page</h2>
+          <Button
+            className={styles.submitBtn}
+            onClick={() => handleSubmit(isSubmitDisabled)}
+            disabled={isSubmitDisabled}
+            data-tool-tip={"Select a brand to proceed"}
+          >
+            Submit
+          </Button>
         </div>
-        <ul className={styles.listContainer}>
-          {allBrands.map((brand => (
-            <Card 
-            key={brand.id}
-            id={brand.id}
-            name={brand.name}
-            logo={brand.logo}
-            isSelected={selectedBrands.includes(brand.id)}
-            onSelect={() => toggleBrand(brand.id)}
-            />
-          )))}
-        </ul>
+        <div className={styles.selectCount}>
+          <Button
+            onClick={toggleSelect}
+          >
+            {selectedBrands.length === allBrands.length ? 'Deselect All' : 'Select All'}
+          </Button>
+          <span className={styles.counter}>Selected: {selectedBrands.length} / {allBrands.length}</span>
+        </div>
       </div>
-    </>
+      <ul className={styles.listContainer}>
+        {allBrands.map((brand => (
+          <Card 
+          key={brand.id}
+          id={brand.id}
+          name={brand.name}
+          logo={brand.logo}
+          isSelected={selectedBrands.includes(brand.id)}
+          onSelect={() => toggleBrand(brand.id)}
+          />
+        )))}
+      </ul>
+    </div>
   );
 }
 
