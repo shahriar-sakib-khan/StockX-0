28eb1ac3eb59c77c990402f54d0b828613a096a5
Ebtehaj Ahmed <ahmedratul216@gmail.com>
2025-03-21 12:56:03 +0600

    Add icons to the dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styles from "./Dashboard.module.css";
 import { NavLink, useNavigate } from "react-router-dom";
+import images from "./Images";
+import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
   const navigate = useNavigate();
@@ -46,48 +47,48 @@ export default function Dashboard() {
 
         <div className={styles.middleSection}>
           <div className={styles.buy}>
-            <img src="images/buy.png" alt="" />
+            <img src={images.img_buy} alt="" />
             <span>Buy</span>
           </div>
           <div className={styles.sell}>
-            <img src="images/sell.png" alt="" />
+            <img src={images.img_sell} alt="" />
             <span>Sell</span>
           </div>
         </div>
 
         <div className={styles.navigationSection}>
           <NavLink to={"/selection"} className={styles.navItem}>
-            {/* <img src="/images/buy.png" /> */}
+            <img src={images.img_selection} />
             <span>Selection</span>
           </NavLink>
 
           <NavLink to={"/inventory"} className={styles.navItem}>
-            {/* <img src="/images/buy.png" /> */}
+            <img src={images.img_inventory} />
             <span>Inventory</span>
           </NavLink>
 
           <NavLink to={"/exchange"} className={styles.navItem}>
-            {/* <img src="/images/buy.png" /> */}
+            <img src={images.img_exchange} />
             <span>Exchange</span>
           </NavLink>
 
           <NavLink to={"/shop"} className={styles.navItem}>
-            {/* <img src="/images/buy.png" /> */}
+            <img src={images.img_shop} />
             <span>Shop</span>
           </NavLink>
 
           <NavLink to={"/exchange-history"} className={styles.navItem}>
-            {/* <img src="/images/buy.png" /> */}
+            <img src={images.img_history} />
             <span>History</span>
           </NavLink>
 
           <NavLink to={"/profile"} className={styles.navItem}>
-            {/* <img src="/images/buy.png" /> */}
+            <img src={images.img_profile} />
             <span>Profile</span>
           </NavLink>
 
           <NavLink to={"/"} className={styles.navItem}>
-            {/* <img src="/images/buy.png" /> */}
+            <img src={images.img_logout} />
             <span>Log out</span>
           </NavLink>
         </div>
---
+++ src/components/Dashboard/Images.jsx
@@ -0,0 +1,23 @@
+import buy from "./images/buy.png";
+import sell from "./images/sell.png";
+import selection from "./images/selection.png";
+import inventory from "./images/inventory.png";
+import exchange from "./images/exchange.png";
+import shop from "./images/shop.png";
+import history from "./images/history.png";
+import profile from "./images/profile.png";
+import logout from "./images/logout.png";
+
+const images = {
+  img_buy: buy,
+  img_sell: sell,
+  img_selection: selection,
+  img_inventory: inventory,
+  img_exchange: exchange,
+  img_shop: shop,
+  img_history: history,
+  img_profile: profile,
+  img_logout: logout,
+};
+
+export default images;
