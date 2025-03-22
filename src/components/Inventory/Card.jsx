f85f94dcc67c66fb2d660db823e36aa46f6c2210
Ebtehaj Ahmed <ahmedratul216@gmail.com>
2025-03-22 16:59:17 +0600

    Mostly prettier reformatting

--- src/components/Inventory/Card.jsx
+++ src/components/Inventory/Card.jsx
@@ -1,49 +1,72 @@
 import { useState } from "react";
-import Modal from './Modal';
-import styles from './Card.module.css';
+import Modal from "./Modal";
+import styles from "./Card.module.css";
 
-function Card({ id, name, type, cardType, picture, price, stock, updateStock }) {
+function Card({
+  id,
+  name,
+  type,
+  cardType,
+  picture,
+  price,
+  stock,
+  updateStock,
+}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalType, setModalType] = useState(null);
 
   const openModal = (type) => {
     setModalType(type);
     setIsModalOpen(true);
-  }
+  };
 
   const closeModal = () => {
     setIsModalOpen(false);
-  }
+  };
 
   const handleStockChange = (value) => {
-    if(modalType === "INCREASE") {
+    if (modalType === "INCREASE") {
       updateStock(id, cardType, type, stock + value);
-    }
-    else if(modalType === "DECREASE" && stock - value >= 0) {
+    } else if (modalType === "DECREASE" && stock - value >= 0) {
       updateStock(id, cardType, type, stock - value);
     }
-  }
+  };
 
   const typeClassName = `type-${type}`;
 
   return (
     <>
-      <div className={`${styles.card} ${cardType === "cylinder" ? styles[typeClassName] : ""}`}>
+      <div
+        className={`${styles.card} ${
+          cardType === "cylinder" ? styles[typeClassName] : ""
+        }`}
+      >
         <span className={styles.stockCount}>Stock: {stock}</span>
         <img src={picture} alt={name} className={styles.image} />
         <h2 className={styles.name}>{name}</h2>
         <p className={styles.price}>Price: Tk {price}</p>
         <div className={styles.controls}>
-          <button onClick={() => openModal("DECREASE")} className={styles.minusBtn} disabled={stock === 0}>-</button>
-          <button onClick={() => openModal("INCREASE")} className={styles.plusBtn}>+</button>
+          <button
+            onClick={() => openModal("DECREASE")}
+            className={styles.minusBtn}
+            disabled={stock === 0}
+          >
+            -
+          </button>
+          <button
+            onClick={() => openModal("INCREASE")}
+            className={styles.plusBtn}
+          >
+            +
+          </button>
         </div>
         <span className={styles.type}>{type}</span>
       </div>
 
-      <Modal 
-        isOpen={isModalOpen} 
-        onClose={closeModal} 
-        onSubmit={handleStockChange} 
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onSubmit={handleStockChange}
         modalType={modalType}
         stock={stock}
       />
--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
-import allBrands from '../../assets/Lists/list_of_brands';
-import Card from './Card';
-import regulators from '../../assets/Lists/regulator_list';
-import stoves from '../../assets/Lists/stove_list';
-import styles from './Inventory.module.css';
-import Navbar from "../Navbar/Navbar";
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import allBrands from "../../assets/Lists/list_of_brands";
+import regulators from "../../assets/Lists/regulator_list";
+import stoves from "../../assets/Lists/stove_list";
+import up_arrow from "./images/up_arrow.png";
+import Card from "./Card";
+import styles from "./Inventory.module.css";
 
 function Inventory() {
-  const { selectedBrands, stockCount, setStockCount, prices } = useOutletContext();
+  const { selectedBrands, stockCount, setStockCount, prices } =
+    useOutletContext();
   const [activeSection, setActiveSection] = useState("cylinders");
   const [scrollPadding, setScrollPadding] = useState(0);
 
@@ -22,8 +23,11 @@ function Inventory() {
       const totalHeight = primaryHeight + secondaryHeight;
 
       setScrollPadding(totalHeight);
-      document.documentElement.style.setProperty("--scroll-padding", `${totalHeight}px`);
-    }
+      document.documentElement.style.setProperty(
+        "--scroll-padding",
+        `${totalHeight}px`
+      );
+    };
 
     updatePadding();
     window.addEventListener("resize", updatePadding);
@@ -32,24 +36,22 @@ function Inventory() {
 
     const handleScroll = () => {
       const top = window.scrollY;
-      
-      sections.forEach(section => {
+
+      sections.forEach((section) => {
         const offset = section.offsetTop - scrollPadding - 100;
         const height = section.offsetHeight;
         const id = section.id;
-        
-        
-        if(top >= offset && top < offset + height)
-          setActiveSection(id);
+
+        if (top >= offset && top < offset + height) setActiveSection(id);
       });
-    }
+    };
 
     window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("resize", updatePadding);
       window.removeEventListener("scroll", handleScroll);
-    }
+    };
   }, [scrollPadding]);
 
   const updateStock = (id, productType, cylinderType, value) => {
@@ -57,91 +59,132 @@ function Inventory() {
 
     setStockCount((prev) => ({
       ...prev,
-      [productType]: { 
-        ...(prev[productType] || {} ),
-        [id]: productType === "cylinder"
-          ? { ...(prev[productType]?.[id] || {}), [cylinderType]: newValue}
-          : newValue,
+      [productType]: {
+        ...(prev[productType] || {}),
+        [id]:
+          productType === "cylinder"
+            ? { ...(prev[productType]?.[id] || {}), [cylinderType]: newValue }
+            : newValue,
       },
     }));
   };
 
   return (
-  <div className={styles.wrapper}>
-    <div className={styles.secondaryNavbar} id="secondary-navbar">
-      <ul className={styles.secondaryNavList}>
-        <li className={styles.secondaryNavItems}><a href="#cylinders" className={`${styles.secondaryNavLink} ${activeSection === "cylinders" ? styles.active : ""}`}>Cylinders</a></li>
-        <li className={styles.secondaryNavItems}><a href="#regulators" className={`${styles.secondaryNavLink} ${activeSection === "regulators" ? styles.active : ""}`}>Regulators</a></li>
-        <li className={styles.secondaryNavItems}><a href="#stoves" className={`${styles.secondaryNavLink} ${activeSection === "stoves" ? styles.active : ""}`}>Stoves</a></li>
-      </ul>
-      <ul className={styles.secondaryNavList}>
-        <li className={styles.secondaryNavItems}><a href="/empty-cylinders" className={styles.secondaryNavLink}>Empty Cylinders</a></li>
-      </ul>
-    </div>
+    <div className={styles.wrapper}>
+      <div className={styles.secondaryNavbar} id="secondary-navbar">
+        <ul className={styles.secondaryNavList}>
+          <li className={styles.secondaryNavItems}>
+            <a
+              href="#cylinders"
+              className={`${styles.secondaryNavLink} ${
+                activeSection === "cylinders" ? styles.active : ""
+              }`}
+            >
+              Cylinders
+            </a>
+          </li>
+          <li className={styles.secondaryNavItems}>
+            <a
+              href="#regulators"
+              className={`${styles.secondaryNavLink} ${
+                activeSection === "regulators" ? styles.active : ""
+              }`}
+            >
+              Regulators
+            </a>
+          </li>
+          <li className={styles.secondaryNavItems}>
+            <a
+              href="#stoves"
+              className={`${styles.secondaryNavLink} ${
+                activeSection === "stoves" ? styles.active : ""
+              }`}
+            >
+              Stoves
+            </a>
+          </li>
+        </ul>
+        <ul className={styles.secondaryNavList}>
+          <li className={styles.secondaryNavItems}>
+            <a href="/empty-cylinders" className={styles.secondaryNavLink}>
+              Empty Cylinders
+            </a>
+          </li>
+        </ul>
+      </div>
 
-    {selectedBrands.length > 0 ? (
-      <section id="cylinders" className={styles.main}>
-        <h1>Cylinders</h1>
+      {selectedBrands.length > 0 ? (
+        <section id="cylinders" className={styles.section}>
+          <h1 className={styles.header}>Cylinders</h1>
+          <div className={styles.grid}>
+            {allBrands
+              .filter((brand) => selectedBrands.includes(brand.id))
+              .map((brand) =>
+                brand.cylinders.map((cylinder) => (
+                  <Card
+                    key={`${brand.id}-${cylinder.type}`}
+                    id={brand.id}
+                    name={brand.name}
+                    type={cylinder.type}
+                    cardType={"cylinder"}
+                    picture={cylinder.image}
+                    price={
+                      prices?.cylinder?.[brand.id]?.[cylinder.type] ??
+                      brand.price
+                    }
+                    stock={
+                      stockCount?.cylinder?.[brand.id]?.[cylinder.type] ??
+                      brand.stock
+                    }
+                    updateStock={updateStock}
+                  />
+                ))
+              )}
+          </div>
+        </section>
+      ) : (
+        <p className={styles.noBrands}>No brands selected</p>
+      )}
+      <section id="regulators" className={styles.section}>
+        <h1 className={styles.header}>Regulators</h1>
         <div className={styles.grid}>
-          {allBrands
-            .filter(brand => selectedBrands.includes(brand.id))
-            .map(brand => (
-              brand.cylinders.map((cylinder) => (
-                <Card
-                  key={`${brand.id}-${cylinder.type}`}
-                  id={brand.id}
-                  name={brand.name}
-                  type={cylinder.type}
-                  cardType={"cylinder"}
-                  picture={cylinder.image}
-                  price={prices?.cylinder?.[brand.id]?.[cylinder.type] ?? brand.price}
-                  stock={stockCount?.cylinder?.[brand.id]?.[cylinder.type] ?? brand.stock}
-                  updateStock={updateStock}
-                />
-              ))
-            ))}
+          {regulators.map((regulator) => (
+            <Card
+              key={`regulator-${regulator.id}`}
+              id={regulator.id}
+              name={regulator.name}
+              cardType={"regulator"}
+              picture={regulator.image}
+              price={prices?.regulator?.[regulator.id] ?? regulator.price}
+              stock={stockCount?.regulator?.[regulator.id] ?? regulator.stock}
+              updateStock={updateStock}
+            />
+          ))}
         </div>
       </section>
-    ) : (
-      <p className={styles.noBrands}>No brands selected</p>
-    )}
-      <section id="regulators" className={styles.main}>
-        <h1>Regulators</h1>
+      <section id="stoves" className={styles.section}>
+        <h1 className={styles.header}>Stoves</h1>
         <div className={styles.grid}>
-        {regulators.map((regulator) => (
-          <Card
-            key={`regulator-${regulator.id}`}
-            id={regulator.id}
-            name={regulator.name}
-            cardType={"regulator"}
-            picture={regulator.image}
-            price={prices?.regulator?.[regulator.id] ?? regulator.price}
-            stock={stockCount?.regulator?.[regulator.id] ?? regulator.stock}
-            updateStock={updateStock}
-          />
-        ))}
+          {stoves.map((stove) => (
+            <Card
+              key={`stove-${stove.id}`}
+              id={stove.id}
+              name={stove.name}
+              cardType={"stove"}
+              picture={stove.image}
+              price={prices?.stove?.[stove.id] ?? stove.price}
+              stock={stockCount?.stove?.[stove.id] ?? stove.stock}
+              updateStock={updateStock}
+            />
+          ))}
         </div>
       </section>
-      <section id="stoves" className={styles.main}>
-        <h1>Stoves</h1>
-        <div className={styles.grid}>
-        {stoves.map((stove) => (
-          <Card
-            key={`stove-${stove.id}`}
-            id={stove.id}
-            name={stove.name}
-            cardType={"stove"}
-            picture={stove.image}
-            price={prices?.stove?.[stove.id] ?? stove.price}
-            stock={stockCount?.stove?.[stove.id] ?? stove.stock}
-            updateStock={updateStock}
-          />
-        ))}
-        </div>
-      </section>
-    <a href="#" className={styles.backToTopBtn}>⬆</a>
-  </div>
-  )
+      <a href="#" className={styles.backToTopBtn}>
+        {/* ⬆ */}
+        <img src={up_arrow} alt="" />
+      </a>
+    </div>
+  );
 }
 
 export default Inventory;
