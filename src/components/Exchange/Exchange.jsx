8c3dcbc315284df6d119ae7c659a9f237769df98
Ebtehaj Ahmed <ahmedratul216@gmail.com>
2025-04-06 19:44:02 +0600

    Hide the print button in exchange page

--- src/components/Exchange/Exchange.jsx
+++ src/components/Exchange/Exchange.jsx
@@ -612,14 +612,14 @@ const Exchange = () => {
 
         {windowWidth < 890 && (
           <div className={styles.buttonContainer}>
-            <Button
+            {/* <Button
               onClick={() => {
                 console.log(deliveredItems);
                 console.log(receivedItems);
               }}
             >
               print
-            </Button>
+            </Button> */}
             <Button
               variant="outline"
               className={`${styles.deliveredBtn} ${styles[activeSection]}`}
