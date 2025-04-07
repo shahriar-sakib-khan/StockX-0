4d899f6e0ae51f914dd1075622b72a79f0a7e4f8
Ebtehaj Ahmed <ahmedratul216@gmail.com>
2025-04-07 23:31:34 +0600

    Add a dummy empty cylinders list for test rendering

--- src/components/EmptyCylinders/EmptyCylinders.jsx
+++ src/components/EmptyCylinders/EmptyCylinders.jsx
@@ -3,6 +3,16 @@ import { useOutletContext } from "react-router-dom";
 function EmptyCylinders() {
   const { EmptyCylinders } = useOutletContext();
 
+  const dummyEmptyList = {
+    1: {
+      "20mm": 5,
+      "22mm": 2,
+    },
+    2: {
+      "22mm": 10,
+    },
+  };
+
   return (
     <>
       <p>Retrieved Empty Cylinders</p>
